7bf6be499d0bfa54489821aee4d612122923bf38
jonyataco <jon.yataco@gmail.com>
2021-03-16 21:06:09 -0400

    Initial Commit

---
+++ LyricRenderNative/LyricRenderNativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LyricRenderNativeApp.swift
+//  LyricRenderNative
+//
+//  Created by Jonathan Yataco  on 3/16/21.
+//
+
+import SwiftUI
+
+@main
+struct LyricRenderNativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
