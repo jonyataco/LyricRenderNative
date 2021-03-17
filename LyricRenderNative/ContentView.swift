ecffdee231a607ee974f12f451d4fe9fca9865e6
jonyataco <jon.yataco@gmail.com>
2021-03-16 21:09:15 -0400

    Initial commit

--- LyricRenderNative/ContentView.swift
+++ LyricRenderNative/ContentView.swift
@@ -8,9 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var lyrics: [String] = []
+    @State var currentLyric: String = ""
+    @State var opacityValue: Double = 0
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            // Sets the background color
+            Color.black.ignoresSafeArea()
+            
+            VStack {
+                if !lyrics.isEmpty {
+                    VStack(alignment: .leading, spacing: 20) {
+                        ForEach(lyrics.indices, id: \.self) { index in
+                            LyricText(index: index + 1, opacity: opacityValue, lyric: lyrics[index])
+                        }
+                    }
+                }
+                TextField(
+                    "Enter your next lyrics",
+                    text: $currentLyric,
+                    onCommit: {
+                        withAnimation(.linear(duration: 0.5)) {
+                            lyrics.append(currentLyric)
+                            opacityValue = Double(lyrics.count)
+                        }
+                        
+                        currentLyric = ""
+                    }
+                )
+            }
+        }
+    }
+}
+
+struct LyricText: View {
+    let index: Int
+    let opacity: Double
+    let lyric: String
+    
+    var body: some View {
+        Text(lyric)
+            .font(Font.custom("Courier", size: 50))
+            .transition(.move(edge: .bottom))
+            .opacity(Double(index) / opacity)
+            .onAppear {
+                print(index, opacity)
+            }
     }
 }
 
@@ -19,3 +63,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
